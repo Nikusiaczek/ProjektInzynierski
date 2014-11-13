ae8737d21d048232072246b4ad959a1b2fe10924
Nikusiaczek <agnieszka.wesolowska1@gmail.com>
2014-11-13 13:10:13 +0100

    dodano okno logowania i podstawe do logowania

---
+++ LoginService/Principal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginService
+{
+    class Principal : IPrincipal
+    {
+        private Identity _identity;
+        public Identity Identity
+        {
+            get { return _identity; }
+            set { _identity = value; }
+        }
+
+        #region IPrincipal Members
+        IIdentity IPrincipal.Identity
+        {
+            get { return this.Identity; }
+        }
+
+        public bool IsInRole(string role)
+        {
+            return _identity.Roles.Contains(role);
+        }
+        #endregion
+    }
+
+}
--- ProjInzynierski/App.xaml.cs
+++ ProjInzynierski/App.xaml.cs
@@ -15,9 +15,30 @@ namespace ProjInzynierski
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-            MainBootstrapper bootstrapper = new MainBootstrapper();
-            bootstrapper.Run();
+            //base.OnStartup(e);
+            //MainBootstrapper bootstrapper = new MainBootstrapper();
+            //bootstrapper.Run();
+
+            Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            LoginScreen logon = new LoginScreen();
+            bool? res = logon.ShowDialog();
+            Application.Current.MainWindow = null;
+            Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+
+            if (res.HasValue && res.Value )//&& Authenticate(logon.UserName, logon.Password))
+            {
+                MainBootstrapper bootStrapper = new MainBootstrapper();
+                bootStrapper.Run();
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Application is exiting due to invalid credentials",
+                    "Application Exit",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                Application.Current.Shutdown(1);
+            }
         }
     }
 }
